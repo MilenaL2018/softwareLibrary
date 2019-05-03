fa8691576232d9044033cc9f386973a2496b25be
milena <milelucero_09@hotmail.com>
2019-05-03 11:25:10 -0300

    Se agregaron las notas a la vista, falta agregar las funciones.

--- managerLibrary/views.py
+++ managerLibrary/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CommentForm
 
 # Create your views here.
+
+
+def capdoc(request):
+    return render(request, 'blog/capdoc.html', {})
+
+def historialnotas(request):
+    if request.method == 'POST':
+        print(request.POST)
+        comment_form = CommentForm(request.POST)
+
+        if comment_form.is_valid():
+            comment_form.save()
+            return redirect()
--- mylibrary/settings.py
+++ mylibrary/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'colegio.apps.SuitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
